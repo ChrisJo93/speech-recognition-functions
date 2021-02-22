e934535152ec7c8cfffa62e2454add02458c3a63
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-22 03:24:25 -0600

    recognition base code set. devdepen installed.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('howdy');
+window.speechRecognition =
+  window.speechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new webkitSpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', (e) => {
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+  console.log(transcript);
+
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+
+    words.appendChild(p);
+  }
+  console.log(transcript);
+  if (transcript.includes('cool')) {
+    console.log('this is the coolest shit omg.');
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
