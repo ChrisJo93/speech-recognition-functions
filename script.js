e9165c67c81e68a17ad8039129c0b9de5c4cbaaf
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-26 00:33:13 -0600

    server runs correctly.

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 // window.speechRecognition =
 //   window.speechRecognition || window.webkitSpeechRecognition;
 
-global.SpeechRecognition =
-  global.SpeechRecognition || global.webkitSpeechRecognition;
-const recognition = new global.SpeechRecognition();
-recognition.interimResults = true;
+// // global.SpeechRecognition =
+// //   global.SpeechRecognition || global.webkitSpeechRecognition;
+// const recognition = new webkitSpeechRecognition();
+// recognition.interimResults = true;
 
-let p = document.createElement('p');
-let date = document.createElement('div');
-const words = document.querySelector('.words');
-words.appendChild(p);
+// let p = document.createElement('p');
+// let date = document.createElement('div');
+// const words = document.querySelector('.words');
+// words.appendChild(p);
 
-recognition.addEventListener('result', (e) => {
-  const transcript = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join('');
+// recognition.addEventListener('result', (e) => {
+//   const transcript = Array.from(e.results)
+//     .map((result) => result[0])
+//     .map((result) => result.transcript)
+//     .join('');
 
-  p.textContent = transcript;
+//   p.textContent = transcript;
 
-  getDate(transcript);
-  getWeather(transcript);
+//   getDate(transcript);
+//   getWeather(transcript);
 
-  if (e.results[0].isFinal) {
-    p = document.createElement('p');
-  }
+//   if (e.results[0].isFinal) {
+//     p = document.createElement('p');
+//   }
 
-  words.appendChild(p);
-});
+//   words.appendChild(p);
+// });
 
-function getDate(transcript) {
-  //requires "cyrus" name. Providing different options to call date
-  const conditions = ['date', 'cyrus what day is it', 'cyrus get date'];
-  //running function if any of these phrases are called
-  if (conditions.some((condition) => transcript.includes(condition))) {
-    console.log(new Date().toString(), 'string');
-    p.textContent = new Date().toString();
-    words.appendChild(p);
-  }
-}
+// function getDate(transcript) {
+//   //requires "cyrus" name. Providing different options to call date
+//   const conditions = ['date', 'cyrus what day is it', 'cyrus get date'];
+//   //running function if any of these phrases are called
+//   if (conditions.some((condition) => transcript.includes(condition))) {
+//     console.log(new Date().toString(), 'string');
+//     p.textContent = new Date().toString();
+//     words.appendChild(p);
+//   }
+// }
 
-function getWeather(transcript) {
-  //need weather api to fetch from
-  if (transcript.includes('weather')) {
-    console.log('fetching weather api');
-  }
-}
+// function getWeather(transcript) {
+//   //need weather api to fetch from
+//   if (transcript.includes('weather')) {
+//     console.log('fetching weather api');
+//   }
+// }
 
-recognition.addEventListener('end', recognition.start);
+// recognition.addEventListener('end', recognition.start);
 
-recognition.start();
+// recognition.start();
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-
 require('dotenv').config();
-
 const app = express();
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
