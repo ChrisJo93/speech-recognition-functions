532e9a576ed07762b2bb54a6cabe2ae2b40d2f05
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-26 00:57:46 -0600

    able to serve html file but lose javascript and css in the process

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
@@ -8,6 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('build'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+console.log(__dirname);
 
 const PORT = process.env.PORT || 5000;
 
