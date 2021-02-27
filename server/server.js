24682fe30734033a7c02b3486be4a9f26fb31fcf
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-26 23:39:46 -0600

    files restructured. Problem almost solved

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('server/public'));
+
+app.get('/', (req, res) => {
+  res.send('You made it champ');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
