f8f2692bba54046d82d1a3ed3090e90e17e39f99
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-27 01:08:44 -0600

    added server comments.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
+// Server construction
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const bodyParser = require('body-parser');
+// Config for environment variables
 require('dotenv').config();
 
+// Json Middleware
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve public folder
 app.use(express.static('server/public'));
 
+// Routes
 app.get('/test', (req, res) => {
   res.send('success. Time to build api call');
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
